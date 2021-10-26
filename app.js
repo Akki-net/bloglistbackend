5e2765c10809f4a58c0f95521c948bb9b07f1476
Akash Sharma <akashsharma.zak@gmail.com>
2021-10-26 19:52:56 +0530

    implemented token-based authentication

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ var express = require('express');
 require('express-async-errors');
 var app = express();
 var middleware = require('./utils/middleware');
+var loginRouter = require('./controllers/login');
+var usersRouter = require('./controllers/users');
 var blogsRouter = require('./controllers/blogs');
 var config = require('./utils/config');
 var logger = require('./utils/logger');
 var mongoose = require('mongoose');
-var usersRouter = require('./controllers/users');
 
 logger.info('connecting to ' + config.MONGODB_URI)
 
@@ -24,6 +25,7 @@ app.use(express.static('build'));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use('/api/login', loginRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/blogs', blogsRouter);
 
