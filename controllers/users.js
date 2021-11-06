80722ae2d55ab93bb23f201a441c2ed98520162a
Akash Sharma <akashsharma.zak@gmail.com>
2021-11-06 20:31:31 +0530

    fix some minor issues

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const usersRouter = require('express').Router();
+const Blog = require('../models/blog');
 const bcrypt = require('bcrypt');
 
 usersRouter.get('/', async (request, response) => {
@@ -11,14 +12,18 @@ usersRouter.post('/', async (request, response) => {
     const body = request.body;
     const saltRounds = 10;
     const passwordHash = await bcrypt.hash(body.password, saltRounds);
+    const blogs = await Blog.find({});
+    const len = blogs.length;
 
     const user = new User({
         username: body.username,
         name: body.name,
-        passwordHash
+        passwordHash,
+        likes: new Array(len).fill(false)
     });
 
     const savedUser = await user.save();
+    
     response.json(savedUser)
 })
 
